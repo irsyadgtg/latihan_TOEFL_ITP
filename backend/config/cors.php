ed183b53b8f18cb69a1934150f6e23302c093839
tharraz <tharraze@gmail.com>
2025-07-05 08:23:56 +0700

    perubahan

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -8,27 +8,28 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
+    | or "CORS". This determines which domains are allowed to access your
+    | application via HTTP requests from a web browser.
     |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    | For a detailed explanation, see:
+    | https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], 
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'], 
 
     'allowed_origins' => ['http://localhost:5174','http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], 
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => true, // Penting jika Anda menggunakan sesi/cookie (misal: Laravel Sanctum)
 
 ];
